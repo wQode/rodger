73d165c4021f6175cd84bf2b7a161908f65400e1
WQU <wqu@tpg.com.au>
2014-07-02 00:01:30 +1000

    last commit for 1st Jul

--- app/controllers/revision.controller.rb
+++ app/controllers/revision.controller.rb
@@ -1,37 +1,13 @@
-class DocumentsController < ApplicationController
-	def index
-		@documents = Document.all
-
+class RevisionController < ApplicationController
+def index
 		@revisions = Revision.all
 	end
 
-	def create
-			@document = Document.new document_params
-			#Add document to current user  * has_and_belongs to many relationship
-			@document.users << @current_user
-			@document.save
-	
-			redirect_to @document
-	end
-
-	def new
-		@document = Document.new
-	end
-
-	def edit
-		@document = Document.find params[:id]
-		# @revision = Revision.find params[:id]
-	end
-
 	def show
-		@document = Document.find params[:id]
-		@users = User.all
-		
 		@revision = Revision.find params[:id]
 	end
 
 	def update
-		document = Document.find params[:id]
 		revision = Revision.new
 
 		revision.document_id = document.id
@@ -47,9 +23,6 @@ class DocumentsController < ApplicationController
 	end
 
 	def destroy
-		document = Document.find params[:id]
-		document.destroy
-
 		revision = Revision.find params[:id]
 		revision.destroy
 		
@@ -57,10 +30,6 @@ class DocumentsController < ApplicationController
 	end
 
 private
-	def document_params
-		params.require(:document).permit(:title, :classification_id, :content)
-	end
-
 	def revision_params
 		params.require(:revision).permit(:title, :content, :classfication_id, :document_id)
 	end
