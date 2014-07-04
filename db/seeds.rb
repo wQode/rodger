df5133a365bcb5cd0766113b9773945db6d07cad
WQU <wqu@tpg.com.au>
2014-07-04 10:16:53 +1000

    final with new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,38 +4,8 @@ Revision.destroy_all
 Classification.destroy_all
 Tag.destroy_all
 
-
-u1 = User.create(:username => 'quma', :avatar => 'x', :password =>'123', :password =>'123')
-u2 = User.create(:username => 'whyte', :avatar => 'x', :password =>'123', :password =>'123', :is_admin => true)
-u3 = User.create(:username => 'rodgers', :avatar => 'x', :password =>'123', :password =>'123')
-
-d1 = Document.create(:title => 'Boeing planes')
-d2 = Document.create(:title => 'Cats')
-d3 = Document.create(:title => 'Stars')
-
-d1 = Document.append(:content => 'Boeing planes can fly really fast')
-d2 = Document.append(:content => 'Cats are furry little balls')
-d3 = Document.append(:content => 'Stars shine so bright')
-
-r1 = Revision.create(:title => 'Revision 1')
-r2 = Revision.create(:title => 'Revision 2')
-r3 = Revision.create(:title => 'Revision 3')
-
-c1 = Classification.create(:title => 'Aviation')
-c2 = Classification.create(:title => 'Animals')
-c3 = Classification.create(:title => 'Astronomy')
-
-d1.revisions << r1
-d1.classification = c1
-d1.tags << t1 << t2
-u1.documents << d1
-
-d2.revisions << r2
-d2.classification = c2
-d2.tags << t2
-u2.documents << d2
-
-d3.revisions << r2
-d3.classification = c3
-d3.tags << t1 << t3
-u3.documents << d3
+u1 = User.create(:username => 'whyte', :avatar => 'http://th07.deviantart.net/fs71/PRE/i/2011/317/a/7/night_elf_druid_by_shadowpriest-d4g28of.jpg', :password =>'whytepassword', :password =>'whytepassword', :is_admin => true)
+u2 = User.create(:username => 'quma', :avatar => 'https://cdn2.content.compendiumblog.com/uploads/user/ab04c84c-11a5-40cf-a34e-fd5aba218b07/52243708-9b44-409d-90ac-e87aa75ff8cd/Image/374bdf821ff36f60c9f2e3e15bc41679/screen_shot_2012_01_17_at_1_32_12_pm_w640.png', :password =>'qumapassword', :password =>'qumapassword')
+u3 = User.create(:username => 'rodgers', :avatar => 'http://www.avatarsdb.com/avatars/king_lion.jpg', :password =>'rodgerspassword', :password =>'rodgerspassword')
+u4 = User.create(:username => 'windar', :avatar => 'http://images.mmorpg.com/images/avatars/Dwarf%20Avatar%202.JPG', :password =>'windarpassword', :password =>'windarpassword')
+u5 = User.create(:username => 'chanel', :avatar => 'http://th08.deviantart.net/fs71/200H/f/2010/251/a/2/mm_mini_tyds_2_by_dedded-d2yala1.png', :password =>'chanelpassword', :password =>'chanelpassword')
